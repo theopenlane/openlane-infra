9842e4b43c1238526ba00b4fdf6184a4ea41c127
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2025-05-30 15:30:22 -0500

    touch readme

--- charts.sh
+++ charts.sh
@@ -2,6 +2,6 @@
 for chart in charts/*; do
   if [[ -d "$chart" ]]; then
     echo "Generating documentation for $chart"
-    helm-docs --chart-search-root "$chart" --output-file "$chart/HELM.md" --template-files "$chart/HELM.md.gotmpl" --skip-version-footer --sort-values-order=file
+    helm-docs --chart-search-root "$chart" --output-file "$chart/HELM.md" --template-files "HELM.md.gotmpl" --skip-version-footer --sort-values-order=file
   fi
 done
